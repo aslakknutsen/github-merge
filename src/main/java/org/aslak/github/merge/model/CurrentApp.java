075fab0466e7ee4e1e4bb079e49da20f272afc6e
Aslak Knutsen <aslak@4fs.no>
2014-12-01 19:59:09 +0100

    Add baseCalbackUrl as app configuration

--- src/main/java/org/aslak/github/merge/model/CurrentApp.java
+++ src/main/java/org/aslak/github/merge/model/CurrentApp.java
@@ -7,32 +7,46 @@ import javax.ejb.Startup;
 @Singleton @Startup
 public class CurrentApp {
 
+    private static final String GITHUB_CLIENT_SECRET = "GITHUB_CLIENT_SECRET";
+    private static final String GITHUB_CLIENT_ID = "GITHUB_CLIENT_ID";
+    private static final String MERGER_CALLBACK_URL = "MERGER_CALLBACK_URL";
+
     private String clientId;
     private String clientSecret;
-    
+    private String baseCallbackUrl;
+
     public CurrentApp() {
     }
-    
+
     public String getClientId() {
         return clientId;
     }
-    
+
     public String getClientSecret() {
         return clientSecret;
     }
-    
+
+    public String getBaseCallbackUrl() {
+        return baseCallbackUrl;
+    }
+
     @PostConstruct
     private void init() {
-        String clientId = System.getenv("GITHUB_CLIENT_ID");
-        String clientSecret = System.getenv("GITHUB_CLIENT_SECRET");
+        String clientId = System.getenv(GITHUB_CLIENT_ID);
+        String clientSecret = System.getenv(GITHUB_CLIENT_SECRET);
+        String baseCallbackUrl = System.getenv(MERGER_CALLBACK_URL);
 
         if(clientId == null || clientId.isEmpty()) {
-            throw new IllegalStateException("GITHUB_CLIENT_ID env variable is missing");
+            throw new IllegalStateException(GITHUB_CLIENT_ID + " env variable is missing");
         }
         if(clientSecret == null || clientSecret.isEmpty()) {
-            throw new IllegalStateException("GITHUB_CLIENT_SECRET env variable is missing");
+            throw new IllegalStateException(GITHUB_CLIENT_SECRET + " env variable is missing");
+        }
+        if(baseCallbackUrl == null || baseCallbackUrl.isEmpty()) {
+            throw new IllegalStateException(MERGER_CALLBACK_URL + " env variable is missing");
         }
         this.clientId = clientId;
         this.clientSecret = clientSecret;
+        this.baseCallbackUrl = baseCallbackUrl;
     }
 }
